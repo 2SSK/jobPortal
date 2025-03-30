65937ab643b4dc105764c8a118fd4ecd9802c187
2SSK <100ravsinghkarmwar@gmail.com>
2025-03-30 18:38:03 +0530

    initialize project structure with React, TypeScript, and Tailwind CSS; add assets and basic components

---
+++ README.md
@@ -0,0 +1,3 @@
+# InsiderJobs : Fullstack Job Portal Website
+
+> Timeline: 8:01
---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-center pt-2">
+        <p className="text-xl font-semibold">Hello, World!</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
