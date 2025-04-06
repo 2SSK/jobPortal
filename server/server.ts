eefb8caaec3a7f83465de20dd7ff5d7ddf65b982
2SSK <100ravsinghkarmwar@gmail.com>
2025-04-06 10:08:09 +0530

    fix: remove unused import and correct Vercel destination to server.ts

--- server/server.ts
+++ server/server.ts
@@ -3,7 +3,6 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/db";
-import "./config/instrument";
 import * as Sentry from "@sentry/node";
 import { clerkWebhooks } from "./controllers/webhooks";
 
