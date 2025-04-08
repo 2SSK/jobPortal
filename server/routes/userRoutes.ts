750cc1fa1ab03865c0138e5eaa3dc6dfca25a405
2SSK <100ravsinghkarmwar@gmail.com>
2025-04-08 21:34:47 +0530

    feat: change job application route to POST and update resume route to use POST method

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -12,13 +12,13 @@ const router = express.Router();
 // Get User Data
 router.get("/user", getUserData);
 
-// Apply for a Job
-router.get("/apply", applyForJob);
+// Apply for a Job 
+router.post("/apply", applyForJob); 
 
 // Get Applied Jobs data
 router.get("/applications", getUserJobApplications);
 
-// Update uesr profile (resume)
-router.get("/update-resume", upload.single("resume"), updateUserResume);
+// Update user profile (resume) - Changed from GET to POST
+router.post("/update-resume", upload.single("resume"), updateUserResume); 
 
 export default router;
